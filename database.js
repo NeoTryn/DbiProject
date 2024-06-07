cd49bd47c18a529e2aec31d9a70143b775c265c3
NeoTryn <eidlerfelix@gmail.com>
2024-06-07 15:51:08 +0200

    idk

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "accounting"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise();
 
 const result = await pool.query("SELECT * FROM Konto WHERE nummer = 2700");
